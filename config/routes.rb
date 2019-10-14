6fd9da78c04f69792ce1d31e89d9dbac1dbc61b9
Mila Zangirolame <milazangirolame@gmail.com>
2019-10-14 17:55:31 -0300

    correcting routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
+require "sidekiq/web"
+
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # Sidekiq Web UI, only for admins.
-  require "sidekiq/web"
+
   authenticate :user, lambda { |u| u.admin } do
     mount Sidekiq::Web => '/sidekiq'
   end
 end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
