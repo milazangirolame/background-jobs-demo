fcdefa4c763091b8004d3404ae2d2b328c7f0b85
Mila Zangirolame <milazangirolame@gmail.com>
2019-10-14 17:48:57 -0300

    configuring routes and application

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+
 Bundler.require(*Rails.groups)
 
 module BackgroundJobsDemo
@@ -15,6 +16,7 @@ module BackgroundJobsDemo
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.active_job.queue_adapter = :sidekiq
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
